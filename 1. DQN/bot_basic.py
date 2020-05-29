b22dda1dd427a58ee3b91d52b5e61c3f46f0ef6d
Strength <brownbearpower@gmail.com>
2020-05-29 21:41:37 +0900

    [fixed] bot_basic.py indent error

--- 1. DQN/bot_basic.py
+++ 1. DQN/bot_basic.py
@@ -41,58 +41,57 @@ print("test from", date_split)
 
 # ENV 설정
 class Environment1:
+    def __init__(self, data, history_t=90):
+        self.data = data
+        self.history_t = history_t
+        self.reset()
+        
+    def reset(self):
+        self.t = 0
+        self.done = False
+        self.profits = 0
+        self.positions = []
+        self.position_value = 0
+        self.history = [0 for _ in range(self.history_t)]
+        return [self.position_value] + self.history # obs
 
-def __init__(self, data, history_t=90):
-	self.data = data
-	self.history_t = history_t
-	self.reset()
-	
-def reset(self):
-	self.t = 0
-	self.done = False
-	self.profits = 0
-	self.positions = []
-	self.position_value = 0
-	self.history = [0 for _ in range(self.history_t)]
-	return [self.position_value] + self.history # obs
-
-def step(self, act):
-	reward = 0
-	
-	# action
-	# 0: Idle
-	# 1: 매수
-	# 2: 매도
-	if act == 1: #매수
-		self.positions.append(self.data.iloc[self.t, :]['Close'])
-	elif act == 2: # 매도
-		if len(self.positions) == 0:
-			reward = -1
-		else:
-			profits = 0
-			for p in self.positions:
-				profits += (self.data.iloc[self.t, :]['Close'] - p)
-			reward += profits
-			self.profits += profits
-			self.positions = []
-	
-	# set next time
-	self.t += 1
-	
-	self.position_value = 0
-	for p in self.positions:
-		self.position_value += (self.data.iloc[self.t, :]['Close'] - p)
-	self.history.pop(0)
-	self.history.append(self.data.iloc[self.t, :]['Close'] - self.data.iloc[(self.t-1), :]['Close'])
-	if (self.t==len(self.data)-1):
-		self.done=True
-	# clipping reward
-	if reward > 0:
-		reward = 1
-	elif reward < 0:
-		reward = -1
-	#print ("t={%d}, done={%str}"%(self.t,self.done))
-	return [self.position_value] + self.history, reward, self.done # obs, reward, done
+    def step(self, act):
+        reward = 0
+        
+        # action
+        # 0: Idle
+        # 1: 매수
+        # 2: 매도
+        if act == 1: #매수
+            self.positions.append(self.data.iloc[self.t, :]['Close'])
+        elif act == 2: # 매도
+            if len(self.positions) == 0:
+                reward = -1
+            else:
+                profits = 0
+                for p in self.positions:
+                    profits += (self.data.iloc[self.t, :]['Close'] - p)
+                reward += profits
+                self.profits += profits
+                self.positions = []
+        
+        # set next time
+        self.t += 1
+        
+        self.position_value = 0
+        for p in self.positions:
+            self.position_value += (self.data.iloc[self.t, :]['Close'] - p)
+        self.history.pop(0)
+        self.history.append(self.data.iloc[self.t, :]['Close'] - self.data.iloc[(self.t-1), :]['Close'])
+        if (self.t==len(self.data)-1):
+            self.done=True
+        # clipping reward
+        if reward > 0:
+            reward = 1
+        elif reward < 0:
+            reward = -1
+        #print ("t={%d}, done={%str}"%(self.t,self.done))
+        return [self.position_value] + self.history, reward, self.done # obs, reward, done
 
 # MODEL
 class Q_Network(nn.Module):
